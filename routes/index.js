c86abe23e5c5bec8178c00152d2fe7b29fad9a33
paulinepasseri <paulinecpasseri@gmail.com>
2025-04-23 11:07:40 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+const APIKEY = process.env.API_KEY
+
+router.get('/movies', (req, res) => {
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${APIKEY}`)
+  .then(response => response.json())
+  .then(data => {
+    res.json({result: true, movies: data})
+  })
+})
+
+module.exports = router;
+
+
+
+
